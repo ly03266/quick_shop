90f0f85a2e0291be8d0a22c70a58b2b85b19d670
ly <15904363005@163.com>
2025-09-16 15:41:43 +0800

    启动禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -109,4 +109,22 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total,records);  //封装并返回分页结果
     }
 
+    /**
+     * 启动禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+        // update employee set status = ? where id = ?
+        /*Employee employee = new Employee();
+        employee.setStatus(status);
+        employee.setId(id);*/
+
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+        employeeMapper.update(employee);
+    }
+
 }
