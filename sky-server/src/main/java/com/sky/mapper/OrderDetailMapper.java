fa1cdc944604f4a379a622ec636636caf86efdee
ly <123.com>
2025-10-02 16:19:39 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 import com.sky.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -12,4 +13,12 @@ public interface OrderDetailMapper {
      * @param orderDetails
      */
     void insertBatch(List<OrderDetail> orderDetails);
+
+    /**
+     * 根据订单ID查询明细数据
+     * @param ordersId
+     * @return
+     */
+    @Select("select * from order_detail where order_id = #{ordersId}")
+    List<OrderDetail> getByOrderId(Long ordersId);
 }
